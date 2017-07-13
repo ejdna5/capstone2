c2606e691a601b2892f541dfdbb20169516e0b60
ejdna5 <ejaehnig@gmail.com>
2017-07-13 00:54:04 +0000

    7-12 update

---
+++ writeRdataframes2file.R
@@ -0,0 +1,11 @@
+write.csv(hn4_scc_table_PotentialImpact, file = "hn4_Routput.csv", na = "NaN")
+write.csv(hn5_scc_table_PotentialImpact, file = "hn5_Routput.csv", na = "NaN")
+write.csv(hn3_scc_table_PotentialImpact, file = "hn3_Routput.csv", na = "NaN")
+
+write.csv(lung2_scc_table_PotentialImpact, file = "lung2_Routput.csv", na = "NaN")
+
+write.csv(cervical_scc_table_PotentialImpact, file = "cervical_Routput.csv", na = "NaN")
+
+write.csv(skin_scc_table_PotentialImpact, file = "skin_Routput.csv", na = "NaN")
+
+write.csv(esoph2_scc_table_PotentialImpact, file = "esoph2_Routput.csv", na = "NaN")
